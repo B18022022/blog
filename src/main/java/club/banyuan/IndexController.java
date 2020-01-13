ec248f6987c39733f2e836c954a5a66e3401e6fb
B18022022 <1017570804@qq.com>
2020-01-13 13:11:11 +0800

    11

--- src/main/java/club/banyuan/IndexController.java
+++ src/main/java/club/banyuan/IndexController.java
@@ -1,5 +1,16 @@
 package club.banyuan;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class IndexController {
+    @RequestMapping ("/users/{username}/blogs/{blogId:[0-9]+}")
+    @ResponseBody
+    public String userProfile(@PathVariable String username, @PathVariable int blogId) {
+        return String.format("user %s id %s", username,blogId);
+    }
 
 }
